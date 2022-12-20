8c66e33011dc31a462789a311a8f5f8ccb43184e
Basavaraj K N <rajiff@users.noreply.github.com>
2022-12-20 11:36:03 +0530

    indented lines

--- EnglishWordChecker.java
+++ EnglishWordChecker.java
@@ -9,6 +9,7 @@ public class EnglishWordChecker {
         try {
             // BufferedReader in = new BufferedReader(new FileReader("/usr/share/dict/american-english")); // For Linux
             BufferedReader in = new BufferedReader(new FileReader("/usr/share/dict/web2")); // For Mac
+            
             String str;
             while ((str = in.readLine()) != null) {
                 if (str.indexOf(word) != -1) {
@@ -17,21 +18,20 @@ public class EnglishWordChecker {
             }
             in.close();
         } catch (IOException e) {
-          System.out.println("Caught an exception " + e);
+            System.out.println("Caught an exception " + e);
         }
-
         return false;
     }
-
+    
     public static void main(String[] args) {
-                // initialize scanner to read from standard input
-                Scanner scanner = new Scanner(System.in);
-
-                System.out.println("Please provide a string to check if it is a valid word ");
-                String inputWord = scanner.next();
-
+        // initialize scanner to read from standard input
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Please provide a string to check if it is a valid word ");
+        String inputWord = scanner.next();
+        
         System.out.println("Given word " + inputWord + " is it a word? " + check_for_word(inputWord));
-
-                scanner.close();
+        
+        scanner.close();
     }
 }
